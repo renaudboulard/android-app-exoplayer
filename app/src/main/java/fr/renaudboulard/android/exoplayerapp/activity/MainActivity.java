941b39f4dded3991896181796f166c1cb8d6da8b
renaudboulard <boulard.renaud@gmail.com>
2015-08-28 22:17:17 +0200

    Add permission Internet

--- app/src/main/java/fr/renaudboulard/android/exoplayerapp/activity/MainActivity.java
+++ app/src/main/java/fr/renaudboulard/android/exoplayerapp/activity/MainActivity.java
@@ -1,6 +1,5 @@
 package fr.renaudboulard.android.exoplayerapp.activity;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
